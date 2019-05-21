03b8d8dac458fa4e187e218849a6bf4b0facd117
Jorlan <jorlanjr@gmail.com>
2019-05-20 22:52:59 -0300

    Adicionado variáveis ambiente

--- src/models/File.js
+++ src/models/File.js
@@ -19,7 +19,8 @@ const File = new mongoose.Schema(
 );
 
 File.virtual('url').get(function () {
-    return `http://localhost:5555/files/${encodeURIComponent(this.path)}`
+    const url = process.env.URL ||'http://localhost:5555';
+    return `${url}/files/${encodeURIComponent(this.path)}`;
 })
 
 module.exports = mongoose.model('File', File);
