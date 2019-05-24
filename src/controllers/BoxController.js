310f420979e4d0c954b5345b2818bf68b50d53aa
Jorlan Junior/LGESP RBrisatc625.juniorD MC PVG(brisatc625.junior@lgepartner.com) <brisatc625.junior@lge.com>
2019-05-24 15:01:16 -0300

    Dependency update

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -2,7 +2,7 @@ const Box = require('../models/Box');
 
 class BoxController {
     async store(req, res){
-     const box = await Box.create(req.body);
+     const box = await Box.create(req.body.title);
 
      return res.json(box);
     }
