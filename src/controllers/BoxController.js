7bcc1b74e7666875544a7684f95bd7b1f8e587c3
Jorlan Junior/LGESP RBrisatc625.juniorD MC PVG(brisatc625.junior@lgepartner.com) <brisatc625.junior@lge.com>
2019-05-24 15:03:56 -0300

    rollback

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -2,7 +2,7 @@ const Box = require('../models/Box');
 
 class BoxController {
     async store(req, res){
-     const box = await Box.create(req.body.title);
+     const box = await Box.create(req.body);
 
      return res.json(box);
     }
